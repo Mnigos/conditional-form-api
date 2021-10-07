2b520c5a1fac7088006770d8e69bf55c58a67af2
Mnigos <makowskiigor@gmail.com>
2021-10-07 18:34:53 +0200

    style(Application): fix ktlint errors

--- src/main/kotlin/com/conditional_form/Application.kt
+++ src/main/kotlin/com/conditional_form/Application.kt
@@ -1,8 +1,9 @@
 package com.conditional_form
 
-import com.conditional_form.plugins.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
+import com.conditional_form.plugins.configureHTTP
+import com.conditional_form.plugins.configureRouting
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
