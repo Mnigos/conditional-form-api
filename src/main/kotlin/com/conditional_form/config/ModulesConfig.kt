a87d6c35ea396e953dcaa33cf2c0f8f3bb01ec59
Mnigos <makowskiigor@gmail.com>
2021-10-10 11:47:50 +0200

    feat(config): create app ModulesConfig

--- src/main/kotlin/com/conditional_form/config/ModulesConfig.kt
+++ src/main/kotlin/com/conditional_form/config/ModulesConfig.kt
@@ -1,2 +1,24 @@
 package com.conditional_form.config
 
+import com.conditional_form.services.IUserImplementation
+import com.conditional_form.services.implementation.UserImplementation
+import com.typesafe.config.ConfigFactory
+import io.ktor.config.HoconApplicationConfig
+import org.kodein.di.DI
+import org.kodein.di.bind
+import org.kodein.di.instance
+import org.kodein.di.singleton
+import org.litote.kmongo.coroutine.CoroutineClient
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.reactivestreams.KMongo
+
+fun DI.MainBuilder.bindMongoServices() {
+    val hoconFile = HoconApplicationConfig(ConfigFactory.load())
+    val mongoUrl = hoconFile.property("mongo.client").getString()
+
+    bind<CoroutineClient>("mongoClient") with singleton { KMongo.createClient(mongoUrl).coroutine }
+}
+
+fun DI.MainBuilder.bindServices() {
+    bind<IUserImplementation>() with singleton { UserImplementation(instance("mongoClient")) }
+}
