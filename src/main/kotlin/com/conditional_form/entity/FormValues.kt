12c3d78fbce3732b722a8bb216241dcb36402b73
Mnigos <makowskiigor@gmail.com>
2021-10-10 10:52:00 +0200

    feat(entity): create FormValues entity

--- src/main/kotlin/com/conditional_form/entity/FormValues.kt
+++ src/main/kotlin/com/conditional_form/entity/FormValues.kt
@@ -1,3 +1,27 @@
 package com.conditional_form.entity
 
-data class FormValues()
+data class FormValues(
+    val courseType: String,
+    val levelOfAdvancement: String,
+    val yourSkills: Array<String>,
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as FormValues
+
+        if (courseType != other.courseType) return false
+        if (levelOfAdvancement != other.levelOfAdvancement) return false
+        if (!yourSkills.contentEquals(other.yourSkills)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = courseType.hashCode()
+        result = 31 * result + levelOfAdvancement.hashCode()
+        result = 31 * result + yourSkills.contentHashCode()
+        return result
+    }
+}
