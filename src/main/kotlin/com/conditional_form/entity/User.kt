078d37a7f9b4055db4466484bc6e75a7af485870
Mnigos <makowskiigor@gmail.com>
2021-10-10 11:01:06 +0200

    refactor: move FormValues from entity to interface

--- src/main/kotlin/com/conditional_form/entity/User.kt
+++ src/main/kotlin/com/conditional_form/entity/User.kt
@@ -1,5 +1,7 @@
 package com.conditional_form.entity
 
+import com.conditional_form.interfaces.FormValues
+
 data class User(
     val _id: String,
     val name: String,
---
+++ src/main/kotlin/com/conditional_form/interfaces/FormValues.kt
@@ -0,0 +1,7 @@
+package com.conditional_form.interfaces
+
+interface FormValues {
+    val courseType: String
+    val levelOfAdvancement: String
+    val yourSkills: Array<String>
+}
