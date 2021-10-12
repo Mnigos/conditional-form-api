7e0d8428e1eb5917cc1b4e41c8f294af5e329eb0
Mnigos <makowskiigor@gmail.com>
2021-10-12 10:25:27 +0200

    feat(UserImplementation): create implementation of methods

--- src/main/kotlin/com/conditional_form/services/implementation/UserImplementation.kt
+++ src/main/kotlin/com/conditional_form/services/implementation/UserImplementation.kt
@@ -1,8 +1,10 @@
 package com.conditional_form.services.implementation
 
 import com.conditional_form.entity.User
+import com.conditional_form.model.response.DefaultResponse
 import com.conditional_form.services.IUserImplementation
 import org.litote.kmongo.coroutine.CoroutineClient
+import org.litote.kmongo.eq
 
 class UserImplementation(mongoClient: CoroutineClient) : IUserImplementation {
 
@@ -12,23 +14,52 @@ class UserImplementation(mongoClient: CoroutineClient) : IUserImplementation {
     private val collectionName = "users"
     private val collection = database.getCollection<User>(collectionName)
 
-    override suspend fun getAllUsers() {
-        TODO("Not yet implemented")
+    override suspend fun getAllUsers(): DefaultResponse<List<User>> {
+        val users = collection.find().toList()
+
+        return DefaultResponse(200, "Success", users)
     }
 
-    override suspend fun getUser(_id: String) {
-        TODO("Not yet implemented")
+    override suspend fun getUser(_id: String): DefaultResponse<User> {
+        val user = collection.findOne(User::_id eq _id)
+
+        return DefaultResponse(200, "Success", user)
     }
 
-    override suspend fun createUser(user: User) {
-        TODO("Not yet implemented")
+    override suspend fun createUser(user: User): DefaultResponse<Boolean> {
+        val newUser = User(
+            name = user.name,
+            formValues = user.formValues
+        )
+
+        return try {
+            collection.insertOne(newUser)
+
+            DefaultResponse(200, "Success", true)
+        } catch (e: Exception) {
+            DefaultResponse(500, "Error", false)
+        }
     }
 
-    override suspend fun updateUser(user: User) {
-        TODO("Not yet implemented")
+    override suspend fun updateUser(user: User): DefaultResponse<User> {
+        return try {
+            user._id?.let { collection.updateOne(it, user) }
+
+            val updatedUser = user._id?.let { collection.findOne(it) }
+
+            DefaultResponse(200, "Success", updatedUser)
+        } catch (e: Exception) {
+            DefaultResponse(500, "Error", null)
+        }
     }
 
-    override suspend fun deleteUser(_id: String) {
-        TODO("Not yet implemented")
+    override suspend fun deleteUser(_id: String): DefaultResponse<Boolean> {
+        return try {
+            collection.deleteOne(User::_id eq _id)
+
+            DefaultResponse(200, "Success", true)
+        } catch (e: Exception) {
+            DefaultResponse(500, "Error", false)
+        }
     }
 }
