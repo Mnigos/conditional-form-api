bae40e50209945229262b111299da8be54e81d30
Mnigos <makowskiigor@gmail.com>
2021-10-12 10:24:42 +0200

    refactor(services): add types for UserImplementation methods

--- src/main/kotlin/com/conditional_form/services/IUserImplementation.kt
+++ src/main/kotlin/com/conditional_form/services/IUserImplementation.kt
@@ -1,11 +1,12 @@
 package com.conditional_form.services
 
 import com.conditional_form.entity.User
+import com.conditional_form.model.response.DefaultResponse
 
 interface IUserImplementation {
-    suspend fun getAllUsers()
-    suspend fun getUser(_id: String)
-    suspend fun createUser(user: User)
-    suspend fun updateUser(user: User)
-    suspend fun deleteUser(_id: String)
+    suspend fun getAllUsers(): DefaultResponse<List<User>>
+    suspend fun getUser(_id: String): DefaultResponse<User>
+    suspend fun createUser(user: User): DefaultResponse<Boolean>
+    suspend fun updateUser(user: User): DefaultResponse<User>
+    suspend fun deleteUser(_id: String): DefaultResponse<Boolean>
 }
