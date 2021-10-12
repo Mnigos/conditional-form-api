53f0925ad964b327865070a503f44dc7dfcda5d9
Mnigos <makowskiigor@gmail.com>
2021-10-12 10:07:37 +0200

    refactor(entity): add _id and createdAt generating in User entity

--- src/main/kotlin/com/conditional_form/entity/User.kt
+++ src/main/kotlin/com/conditional_form/entity/User.kt
@@ -1,9 +1,14 @@
 package com.conditional_form.entity
 
 import com.conditional_form.`interface`.FormValues
+import org.bson.codecs.pojo.annotations.BsonId
+import java.time.LocalDateTime
+import java.util.UUID
 
 data class User(
-    val _id: String,
+    @BsonId
+    val _id: String = UUID.randomUUID().toString(),
     val name: String,
     val formValues: FormValues,
+    val createdAt: LocalDateTime = LocalDateTime.now()
 )
